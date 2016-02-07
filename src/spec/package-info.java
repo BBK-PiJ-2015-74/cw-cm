e13eafbd89a85756f5c7a1f0e32ccf032e1a1e40
LucieCBurgess <lucie.burgess@icloud.com>
2016-02-07 20:10:12 +0000

    First commit of Coursework 3 Added package spec (with interfaces), package test, package impl

---
+++ src/spec/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author BBK-PiJ-2015-74 lburge01
+ * Coursework 3 Contact Manager spec (interfaces provided with coursework)
+ */
+package spec;
---
+++ src/test/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author BBK-PiJ-2015-74 lburge01
+ * Coursework 3 Contact Manager tests
+ *
+ */
+package test;

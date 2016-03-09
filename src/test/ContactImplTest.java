3d11361beb1d9d5a97f41192ffe220e5ec337e58
LucieCBurgess <lucie.burgess@icloud.com>
2016-03-09 12:11:02 +0000

    Tidied up tests relating to constructors Amended tests to deal with NullPointer/ Illegal Argument exceptions

--- src/test/ContactImplTest.java
+++ src/test/ContactImplTest.java
@@ -17,21 +17,21 @@ import spec.Contact;
  */
 public class ContactImplTest {
 	
-	private Contact testCt1;
-    private Contact testCt2;
-    private String testName;
+	private Contact testCt1, testCt2;
+    private String testName, testName2;
     private String testNotes1, testNotes2;
     private int testID;
 	
     @Before
     public void setUp() {
         testID = 10;
-        testName = "Humpty Dumpty";
+        testName = "Humpty_Dumpty";
+        testName2 = "Moppet the Kitten";
         testNotes1 = "He's a good egg";
         testNotes2 = "All the king's horses and all the king's men couldn't put Humpty back together again";
 
         testCt1 = new ContactImpl(testID, testName, testNotes1);
-        testCt2 = new ContactImpl(testID, testName, testNotes2);
+        testCt2 = new ContactImpl(testID, testName2, testNotes2);
     }
 	
     
@@ -98,6 +98,7 @@ public class ContactImplTest {
     /**
      * Test whether constructor is implemented correctly - null notes
      * won't work until getNotes() is implemented
+     * should throw NullPointerException for null notes?
      */
     @Test
     public void testConstructorNullNotes() {
@@ -129,7 +130,7 @@ public class ContactImplTest {
 	 * This test should pass because testID has been defined as an int
 	 */
 	@Test 
-	public void contactGetIdReturnsIntTest() {
+	public void testContactGetIdReturnsInt() {
 		assertTrue (testCt1.getId() == (int)testCt1.getId());
 	}
     
@@ -138,34 +139,43 @@ public class ContactImplTest {
 	 * This test will fail until getId() implemented
 	 */
 	@Test 
-	public void contactGetIdReturnsID() {
+	public void testContactGetIdReturnsID() {
 		assertEquals (testID, testCt1.getId());
 	}
 	
 	/**
-	 * Tests whether GetName() returns expected string
-	 * Fails before Contact is implemented (testing interface only)
+	 * Tests whether getName() returns expected string
+	 * This test will fail until getName() implemented
 	 */
 	@Test
-	public void contactGetNameReturnsStringTest() {
-		fail("Not yet implemented");
+	public void testContactGetNameReturnsExpectedName() {
+		assertEquals (testName, testCt1.getName());
 	}
 	
 	/**
-	 * Tests whether GetNotes() returns expected string
-	 * Fails before Contact is implemented (testing interface only)
+	 * Test whether getName() returns a name with spaces included
+	 * This test will fail until getName() is implemented
 	 */
 	@Test
-	public void contactGetNotesReturnsStringTest() {
-		fail("Not yet implemented");
+	public void testContactGetNameWithSpaces() {
+		assertEquals (testName2, testCt2.getName());
 	}
 	
 	/**
-	 * Tests whether addNotes(note) takes parameter (note) correctly
+	 * Tests whether getNotes() returns expected string
+	 * This test will fail until getNotes() is implemented
+	 */
+	@Test
+	public void testContactGetNotesReturnsNotes() {
+		assertEquals (testNotes2, testCt2.getNotes());
+	}
+	
+	/**
+	 * Tests whether addNotes(note) takes argument (note) correctly
 	 * Fails before Contact is implemented (testing interface only)
 	 */
 	@Test
-	public void contactAddNotesAcceptsParameterNoteTest() {
+	public void contactAddNotesAcceptsArgumentNote() {
 		fail("Not yet implemented");
 	}
 	
@@ -173,6 +183,7 @@ public class ContactImplTest {
     public void tearDown() {
 		testID = 0;
         testName = null;
+        testName2 = null;
         testNotes1 = null;
         testNotes2 = null;
         testCt1 = null;

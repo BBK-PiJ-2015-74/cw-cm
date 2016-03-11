3b1fb3f0f9f30323b1d6699c9390f8b97151a1b8
LucieCBurgess <lucie.burgess@icloud.com>
2016-03-11 21:41:32 +0000

    Skeleton implementation of PastMeeting added, called PastMeetingImpl. Constructor and methods not yet implemented

---
+++ src/impl/PastMeetingImpl.java
@@ -0,0 +1,57 @@
+package impl;
+
+import java.util.Calendar;
+import java.util.Set;
+
+import spec.Contact;
+import spec.PastMeeting;
+
+/**
+ * 
+ * @author BBK-PiJ-2015-74 lburge01
+ * Implementation of interface PastMeeting
+ */
+
+public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+	
+	/**
+	 * The class implementing this interface has one constructor with four parameters:
+	 * an ID (int), a date, a set of contacts that must be non-empty, and a String containing the notes for the meetin
+	 * @param int meetingID the id of the meeting
+	 * @param Calendar meetingDate the date of the meeting
+	 * @param Set<Contact> meetingDelegates the set of contacts that attended the meeting
+	 * @param ArrayList<String> meetingNotes the notes from the meeting
+	 * 
+	 * @throws NullPointerException if meetingDate, meetingDelegates, meetingNotes is null
+	 * @throws IllegalArgumentException if id is negative or zero or if contacts is empty
+	 */
+
+	public PastMeetingImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Calendar getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Contact> getContacts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNotes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
